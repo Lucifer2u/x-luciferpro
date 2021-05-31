16092444acbf274ede1130744d54c42d3d3900ab
lucifer <xiaoweliang@qq.com>
2021-05-31 20:31:13 +0800

    登录信息配置

---
+++ src/main/java/org/lucifer/vbluciferpro/controller/LoginController.java
@@ -0,0 +1,15 @@
+package org.lucifer.vbluciferpro.controller;
+
+import org.lucifer.vbluciferpro.model.RespBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public RespBean login(){
+        return RespBean.error("尚未登录，请登录");
+    }
+
+}
