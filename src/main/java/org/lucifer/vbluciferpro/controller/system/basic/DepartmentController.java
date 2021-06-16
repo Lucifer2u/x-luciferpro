b311253a444ca141e77933474b3c14fd041c3e0d
lucifer <xiaoweliang@gmail.com>
2021-06-16 17:42:08 +0800

    部门管理模块设计

---
+++ src/main/java/org/lucifer/vbluciferpro/controller/system/basic/DepartmentController.java
@@ -0,0 +1,48 @@
+package org.lucifer.vbluciferpro.controller.system.basic;
+
+import org.lucifer.vbluciferpro.model.Department;
+import org.lucifer.vbluciferpro.model.RespBean;
+import org.lucifer.vbluciferpro.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/system/basic/department")
+public class DepartmentController {
+
+    @Autowired
+    DepartmentService departmentService;
+
+    @GetMapping("/")
+    public List<Department> getAllDepartments(){
+        return departmentService.getAllDepartments();
+    }
+
+    //调用存储过程
+    @PostMapping("/")
+    public RespBean addDep(@RequestBody Department dep) {
+        departmentService.addDep(dep);
+        if (dep.getResult() == 1) {
+            return RespBean.ok("添加成功", dep);
+        }
+        return RespBean.error("添加失败");
+    }
+
+    @DeleteMapping("/{id}")
+    public RespBean deleteDepById(@PathVariable Integer id) {
+        Department dep = new Department();
+        dep.setId(id);
+        departmentService.deleteDepById(dep);
+        if (dep.getResult() == -2) {
+            return RespBean.error("该部门下有子部门，删除失败");
+        } else if (dep.getResult() == -1) {
+            return RespBean.error("该部门下有员工，删除失败");
+        } else if (dep.getResult() == 1) {
+            return RespBean.ok("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
+
+}
