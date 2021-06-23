8ecd83cffcf9874066b681df52004161c6c94660
lucifer <xiaoweliang@gmail.com>
2021-06-23 20:08:12 +0800

    自动计算合同期限

--- src/main/java/org/lucifer/vbluciferpro/service/EmployeeService.java
+++ src/main/java/org/lucifer/vbluciferpro/service/EmployeeService.java
@@ -6,6 +6,8 @@ import org.lucifer.vbluciferpro.model.RespPageBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +16,10 @@ public class EmployeeService {
 
     @Autowired
     EmployeeMapper employeeMapper;
+
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+    SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+    DecimalFormat decimalFormat = new DecimalFormat("##.00");
 
 
     public RespPageBean getEmployeeByPage(Integer page, Integer size, String keyword) {
@@ -29,6 +35,12 @@ public class EmployeeService {
     }
 
     public Integer addEmp(Employee employee) {
+
+        Date beginContract = employee.getBeginContract();
+        Date endContract = employee.getEndContract();
+        double month = (Double.parseDouble(yearFormat.format(endContract)) - Double.parseDouble(yearFormat.format(beginContract))) * 12
+                + (Double.parseDouble(monthFormat.format(endContract)) - Double.parseDouble(monthFormat.format(beginContract)));
+        employee.setContractTerm(Double.parseDouble(decimalFormat.format(month / 12)));
         return employeeMapper.insertSelective(employee);
     }
 
